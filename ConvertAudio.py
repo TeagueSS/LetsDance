f4c805071dcad5ba04a306c2c5e40bbcb791682a
teaguesangster <sangsterteague@yahoo.com>
2024-11-04 18:54:01 -0800

    Getting the beat map from an imput file

--- ConvertAudio.py
+++ ConvertAudio.py
@@ -1,1 +1,20 @@
 import librosa
+import numpy as np
+
+def loadFile(audio_path):
+    # Load the audio file
+    y, sr = librosa.load(audio_path, sr=None)
+
+    # Get tempo and beat frames
+    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+    beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+
+    print("Tempo:", tempo)
+    print("Beat times:", beat_times)
+    return y, sr
+
+
+print(loadFile("/Users/teaguesangster/Code/Python/CS450/DataSetup/downloads/Just Dance Hits： Only Girl (In The World) by Rihanna [12.9k]_audio.mp3"))
+
+
+
