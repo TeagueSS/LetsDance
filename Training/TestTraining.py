8e11c5922f5d53c1d6ff33dcff8f6d23604552f6
teaguesangster <sangsterteague@yahoo.com>
2024-12-01 16:41:47 -0800

    Updating Model Training and testing to see if the model runs correctly:

---
+++ Training/TestTraining.py
@@ -0,0 +1,142 @@
+import unittest
+import numpy as np
+import os
+from tensorflow.keras.models import load_model
+
+# Import your functions here
+from Training import combine_npz_files, preprocess_data, build_rnn_model, train_rnn_model, evaluate_and_predict, predict_body_mappings
+
+# Create mock data for testing
+import numpy as np
+import os
+
+
+def create_mock_npz_files(test_directory, num_files=2, num_samples=20, audio_features=5, frame_features=3):
+    if not os.path.exists(test_directory):
+        os.makedirs(test_directory)
+
+    for i in range(num_files):
+        audio_data = np.random.rand(num_samples, audio_features)
+        frame_data = np.random.rand(num_samples, frame_features)
+        file_path = os.path.join(test_directory, f"test_data_{i}.npz")
+        np.savez(file_path, audio=audio_data, frame=frame_data)
+    print(f"Created {num_files} mock .npz files in {test_directory}")
+
+
+# Create mock data in the 'test_data' directory
+create_mock_npz_files('test_data')
+
+
+class TestRNNPipeline(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Paths for test data and model
+        cls.test_data_dir = 'test_data'
+        cls.model_save_path = 'test_model.h5'
+
+        # Create mock data
+        create_mock_npz_files(cls.test_data_dir, num_files=2, num_samples=20, audio_features=5, frame_features=3)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Clean up created files
+        import shutil
+        shutil.rmtree(cls.test_data_dir)
+        if os.path.exists(cls.model_save_path):
+            os.remove(cls.model_save_path)
+
+    def test_combine_npz_files(self):
+        # Test combining .npz files
+        audio_data, frame_data = combine_npz_files(self.test_data_dir)
+        self.assertEqual(audio_data.shape, (40, 5))
+        self.assertEqual(frame_data.shape, (40, 3))
+        print("combine_npz_files passed.")
+
+    def test_preprocess_data(self):
+        # Use data from combine_npz_files
+        audio_data, frame_data = combine_npz_files(self.test_data_dir)
+        time_steps = 5
+        X_audio, X_prev_frame, y_next_frame, audio_scaler, frame_scaler = preprocess_data(audio_data, frame_data, time_steps)
+
+        # Check shapes
+        expected_samples = audio_data.shape[0] - time_steps - 1
+        self.assertEqual(X_audio.shape, (expected_samples, time_steps, audio_data.shape[1]))
+        self.assertEqual(X_prev_frame.shape, (expected_samples, frame_data.shape[1]))
+        self.assertEqual(y_next_frame.shape, (expected_samples, frame_data.shape[1]))
+        print("preprocess_data passed.")
+
+    def test_build_rnn_model(self):
+        # Build model and check its structure
+        time_steps = 5
+        audio_features = 5
+        frame_features = 3
+        audio_input_shape = (time_steps, audio_features)
+        frame_input_shape = (frame_features,)
+
+        model = build_rnn_model(audio_input_shape, frame_input_shape)
+        self.assertIsNotNone(model)
+        self.assertEqual(len(model.inputs), 2)
+        self.assertEqual(model.output_shape, (None, frame_features))
+        print("build_rnn_model passed.")
+
+    def test_train_rnn_model(self):
+        # Prepare data
+        audio_data, frame_data = combine_npz_files(self.test_data_dir)
+        time_steps = 5
+        X_audio, X_prev_frame, y_next_frame, audio_scaler, frame_scaler = preprocess_data(audio_data, frame_data, time_steps)
+        audio_input_shape = (X_audio.shape[1], X_audio.shape[2])
+        frame_input_shape = (X_prev_frame.shape[1],)
+
+        # Build model
+        model = build_rnn_model(audio_input_shape, frame_input_shape)
+
+        # Train model
+        history, X_val, y_val = train_rnn_model(model, X_audio, X_prev_frame, y_next_frame, self.model_save_path, epochs=1, batch_size=4)
+
+        # Check if the model was saved
+        self.assertTrue(os.path.exists(self.model_save_path))
+        print("train_rnn_model passed.")
+
+    def test_evaluate_and_predict(self):
+        # Prepare data
+        audio_data, frame_data = combine_npz_files(self.test_data_dir)
+        time_steps = 5
+        X_audio, X_prev_frame, y_next_frame, audio_scaler, frame_scaler = preprocess_data(audio_data, frame_data, time_steps)
+        audio_input_shape = (X_audio.shape[1], X_audio.shape[2])
+        frame_input_shape = (X_prev_frame.shape[1],)
+
+        # Build and train model
+        model = build_rnn_model(audio_input_shape, frame_input_shape)
+        history, X_val, y_val = train_rnn_model(model, X_audio, X_prev_frame, y_next_frame, self.model_save_path, epochs=1, batch_size=4)
+
+        # Evaluate and predict
+        evaluate_and_predict(model, X_val, y_val, frame_scaler, num_predictions=2)
+        print("evaluate_and_predict passed.")
+
+    def test_predict_body_mappings(self):
+        # Prepare data
+        audio_data, frame_data = combine_npz_files(self.test_data_dir)
+        time_steps = 5
+        X_audio, X_prev_frame, y_next_frame, audio_scaler, frame_scaler = preprocess_data(audio_data, frame_data, time_steps)
+        audio_input_shape = (X_audio.shape[1], X_audio.shape[2])
+        frame_input_shape = (X_prev_frame.shape[1],)
+
+        # Build and train model
+        model = build_rnn_model(audio_input_shape, frame_input_shape)
+        history, _, _ = train_rnn_model(model, X_audio, X_prev_frame, y_next_frame, self.model_save_path, epochs=1, batch_size=4)
+
+        # Prepare input for prediction
+        audio_input_array = X_audio[0]  # Use first sequence
+        initial_frame = X_prev_frame[0]  # Use corresponding previous frame
+        num_predictions = 2
+
+        predicted_frames = predict_body_mappings(
+            self.model_save_path, audio_scaler, frame_scaler, audio_input_array, initial_frame, num_predictions
+        )
+
+        self.assertEqual(predicted_frames.shape, (num_predictions, frame_data.shape[1]))
+        print("predict_body_mappings passed.")
+
+if __name__ == '__main__':
+    unittest.main()
--- Training/Training.py
+++ Training/Training.py
@@ -7,8 +7,9 @@ from tensorflow.keras.layers import LSTM, Dense, Concatenate, Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import ModelCheckpoint
 
-
 # Load and Combine Data
+# Here we have all of our idividual trainings, and if we get more we can find them
+# Here
 def combine_npz_files(input_directory):
     """
     Combine multiple .npz files from a directory into a single dataset.
@@ -38,76 +39,74 @@ def combine_npz_files(input_directory):
     return combined_audio_data, combined_frame_data
 
 
-# Preprocess Data for Training
 def preprocess_data(audio_data, frame_data, time_steps=10):
     """
     Preprocess audio and frame data to create time-step sequences for training an RNN.
+    Here we have to make our valid "Sequences" For our data to use for trianing.
+
     """
-    # Normalize the audio data only
-    # (because our body values will be between 0 and 1 for all indecies ->)
-
-    #Building our frame scaler ->
-    frame_scaler = MinMaxScaler()
-    frame_data_normalized = frame_scaler.fit_transform(frame_data)
-
+    # Normalzing our Audio data so we can ensure it trains in the correct way
+    # and also reverses in the correct format ->
     audio_scaler = MinMaxScaler()
     audio_data_normalized = audio_scaler.fit_transform(audio_data)
 
+    # Normalize the frame data (even if it's between 0 and 1)
+    # We normaize it again as we can use our normalizer to reverse our encoding later on ->
 
-    # Then we build our 3 sub sections based off of this ->
+
+    frame_scaler = MinMaxScaler()
+    frame_data_normalized = frame_scaler.fit_transform(frame_data)
+
     # Create sequences of time steps for audio input, previous frame input, and frame output
+    # Here our samples are everything except for the first one for our Frames,
+    # (We want our model to always know where to start the dance)
     num_samples = audio_data_normalized.shape[0] - time_steps - 1
     audio_sequences, previous_frames, next_frames = [], [], []
 
-    # Looping through all of our samples for this training window ->
+    # Loop through all of our samples for this training window
     for i in range(num_samples):
-        # For our audio window, it's every audio timings :
+        # For our audio window, it's every audio timing:
         audio_sequences.append(audio_data_normalized[i:i + time_steps])
-        # For our Frame data it's the same but missing the first
-        # Because there's no Guarente it has a starting position ->
+        # Previous frame (where the model is currently standing)
         previous_frames.append(frame_data_normalized[i + time_steps - 1])
-        # Then here is sequence of next frames it's meant to predict ->
+        # Next frame to predict
         next_frames.append(frame_data_normalized[i + time_steps])
 
-    # Converting these "Frames" into arrays that can be fed into our model
+    # Convert these "Frames" into arrays that can be fed into our model
     X_audio = np.array(audio_sequences)
     X_prev_frame = np.array(previous_frames)
     y_next_frame = np.array(next_frames)
 
-    #Returning our arrays of frames:
+    # Return our arrays of frames and scalers
     return X_audio, X_prev_frame, y_next_frame, audio_scaler, frame_scaler
-
 
 # Build the RNN Model
 def build_rnn_model(audio_input_shape, frame_input_shape):
     """
-    Here we have seperate input layers, as we have different information for our context:
+    Build and compile an RNN model to map audio features and previous frames to predict the next frame.
+    Here each one of our layers are a differenent part of our date we either need to
+    encode or Decode ->
     """
-    # Audio Layer ->
-    # Here we are passing our audio frame shape and naming it
+    # Audio input layer
     audio_input = Input(shape=audio_input_shape, name='audio_input')
-    # Using a LSTM for our Nuerons as we want to see the changes in our song
-    # And actually keep track of how our audio is changing overtime
-    lstm_out = LSTM(100, activation='tanh')(audio_input)
+    # LSTM layer to process audio sequences
+    lstm_out = LSTM(100)(audio_input)
 
-    # Second layer for previous frame (So we know where our model is standing)
+    # Previous frame input layer
     prev_frame_input = Input(shape=frame_input_shape, name='previous_frame_input')
 
-    # Combination Layer (Where our previous Skeletal Stance and CURENT audio get combined)
+    # Concatenate LSTM output and previous frame input
     concat = Concatenate()([lstm_out, prev_frame_input])
 
-    # Our predicition layer of the RNN
-    # we are using a dense layer as we want to represent the entire shape of the body
-    # and give each body part it's own neuron to learn from
+    # Dense layer to predict next frame
     output = Dense(frame_input_shape[0], activation='linear')(concat)
 
-    # Finally turning all of our layers into one by "Compling" our model ->
+    # Build and compile the model
     model = Model(inputs=[audio_input, prev_frame_input], outputs=output)
     model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
-    #Returning the model (Layers) we just made
+    # Return the model
     return model
-
 
 # Train the RNN Model with Model Checkpointing
 def train_rnn_model(model, X_audio, X_prev_frame, y_next_frame, output_model_path, epochs=50, batch_size=32):
@@ -138,14 +137,15 @@ def train_rnn_model(model, X_audio, X_prev_frame, y_next_frame, output_model_pat
 
     return history, [X_audio_val, X_prev_frame_val], y_val
 
-
 # Evaluate and Make Predictions
 def evaluate_and_predict(model, X_val, y_val, frame_scaler, num_predictions=5):
     """
     Evaluate the trained model and make predictions on the validation set.
     """
     # Evaluate the model on the validation set
+    # Here we predict our loss (How far off) we are from our desired entries
     val_loss, val_mae = model.evaluate(X_val, y_val, verbose=1)
+    # We pring it out as we train so we can see these numbers hopefully go down overtime ->
     print(f"Validation Loss: {val_loss}")
     print(f"Validation MAE: {val_mae}")
 
@@ -153,6 +153,8 @@ def evaluate_and_predict(model, X_val, y_val, frame_scaler, num_predictions=5):
     predictions = model.predict(X_val)
 
     # Invert scaling to interpret predictions in the original scale
+    # Here because we compressed our data so we can train off of it,
+    # We let it scale back up so we can actually Visulaize our results ->
     predictions_unscaled = frame_scaler.inverse_transform(predictions)
     y_val_unscaled = frame_scaler.inverse_transform(y_val)
 
@@ -162,22 +164,23 @@ def evaluate_and_predict(model, X_val, y_val, frame_scaler, num_predictions=5):
         print(f"Predicted Frame {i + 1}: {predictions_unscaled[i]}")
         print(f"Actual Frame {i + 1}: {y_val_unscaled[i]}")
 
-
 # Main Function to Run the Entire Pipeline
 if __name__ == "__main__":
     # Define paths for loading and saving data and models
-    # Path for all of our data:
+    # Replace with your actual paths
     input_directory = "/path/to/your/npz/files"
-    # Path for our training weights
     output_model_path = "/path/to/your/saved_audio_to_frame_model_with_context.h5"
 
     # Load and combine data
     audio_data, frame_data = combine_npz_files(input_directory)
 
     # Preprocess the data to create sequences for the RNN
-    time_steps = 10  # Number of time steps to look back
-    X_audio, X_prev_frame, y_next_frame, audio_scaler, frame_scaler = preprocess_data(audio_data, frame_data,
-                                                                                      time_steps)
+    # Here we might need to increase this depending on how many frames of a dance sequence
+    # We want to pass it ->
+    time_steps = 10
+    X_audio, X_prev_frame, y_next_frame, audio_scaler, frame_scaler = preprocess_data(
+        audio_data, frame_data, time_steps
+    )
 
     # Build the RNN model
     audio_input_shape = (X_audio.shape[1], X_audio.shape[2])  # Shape is (time_steps, audio_features)
@@ -187,17 +190,16 @@ if __name__ == "__main__":
     # Train the RNN model with checkpointing
     epochs = 50
     batch_size = 32
-    history, X_val, y_val = train_rnn_model(model, X_audio, X_prev_frame, y_next_frame, output_model_path, epochs,
-                                            batch_size)
+    history, X_val, y_val = train_rnn_model(
+        model, X_audio, X_prev_frame, y_next_frame, output_model_path, epochs, batch_size
+    )
 
     # Evaluate the model and make predictions
     evaluate_and_predict(model, X_val, y_val, frame_scaler)
 
-import numpy as np
-from tensorflow.keras.models import load_model
-from sklearn.preprocessing import MinMaxScaler
-
-
+# Prediction Function
+# Here we can provide audio and actually see our model return frames
+# Which we can then later Visualize through Predict Body mappings
 def predict_body_mappings(model_path, audio_scaler, frame_scaler, audio_input_array, initial_frame, num_predictions):
     """
     Predict body mappings for given audio input sequences using a pre-trained model.
@@ -220,36 +222,37 @@ def predict_body_mappings(model_path, audio_scaler, frame_scaler, audio_input_ar
     # Normalize the audio input using the scaler
     audio_input_normalized = audio_scaler.transform(audio_input_array)
 
+    # Normalize the initial frame
+    previous_frame_normalized = frame_scaler.transform([initial_frame])[0]
+
     # Prepare an empty list to collect predicted frames
     predicted_frames = []
-
-    # Set the initial previous frame to the given initial frame
-    previous_frame = initial_frame
 
     # Predict body frames one by one
     for i in range(num_predictions):
         # Add a batch dimension to the input (since the model expects batched input)
         audio_input_reshaped = np.expand_dims(audio_input_normalized, axis=0)  # Shape (1, num_timesteps, features)
-        previous_frame_reshaped = np.expand_dims(previous_frame, axis=0)  # Shape (1, features)
+        prev_frame_reshaped = np.expand_dims(previous_frame_normalized, axis=0)  # Shape (1, features)
 
         # Make a prediction
-        predicted_frame_normalized = model.predict([audio_input_reshaped, previous_frame_reshaped])
+        predicted_frame_normalized = model.predict([audio_input_reshaped, prev_frame_reshaped])
 
-        # Invert the normalization on the predicted frame
+        # Invert scaling to get the predicted frame in original scale
         predicted_frame = frame_scaler.inverse_transform(predicted_frame_normalized)
 
         # Add the predicted frame to the list
         predicted_frames.append(predicted_frame.flatten())
 
         # Update the previous frame for the next prediction
-        previous_frame = predicted_frame.flatten()
+        previous_frame_normalized = predicted_frame_normalized.flatten()
 
         # Optional: Shift the audio input for continuous prediction
         # Assuming new audio information is available, you could shift the audio input to always keep `num_timesteps`.
         # Here, we use the existing sequence as an example.
         if i + 1 < len(audio_input_normalized):
             audio_input_normalized = np.roll(audio_input_normalized, -1, axis=0)  # Shift one time step to the left
-            audio_input_normalized[-1] = audio_input_normalized[-1]  # Optional: Add new audio features if available
+            # Optionally, replace the last element with new audio features
+            # audio_input_normalized[-1] = new_audio_features_normalized
 
     # Convert list to ndarray for consistency
     predicted_frames = np.array(predicted_frames)
